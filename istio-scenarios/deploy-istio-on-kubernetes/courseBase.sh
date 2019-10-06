4c73c3c02a189550cf9b6728cebd1a6ea4000229
Anil Dhulappanavar <anil.dhulappanavar.2015@gmail.com>
2019-10-06 14:08:38 +0530

    Updated CourseBase to download yaml files

--- istio-scenarios/deploy-istio-on-kubernetes/courseBase.sh
+++ istio-scenarios/deploy-istio-on-kubernetes/courseBase.sh
@@ -1,2 +1,2 @@
-ssh root@host01 "git clone https://github.com/katacoda-scenarios/istio-katacoda-scenarios.git; mv istio-katacoda-scenarios/assets istio; rm -rf istio-katacoda-scenarios"
+ssh root@host01 "git clone https://github.com/adhulappanavar/katacoda-scenarios.git; mv katacoda-scenarios/istio/assets/ istio; rm -rf katacoda-scenarios"
 ssh root@host01 "sed -i 's/\$HOST_IP/[[HOST_IP]]/g' istio/all.yaml; sed -i 's/\$HOST_IP/[[HOST_IP]]/g' istio/istio.yaml; sed -i 's/\$HOST_IP/[[HOST_IP]]/g' istio/istio-auth.yaml; sed -i 's/\$HOST_IP/[[HOST_IP]]/g' istio/addons/grafana.yaml; sed -i 's/\$HOST_IP/[[HOST_IP]]/g' istio/addons/zipkin.yaml; sed -i 's/\$HOST_IP/[[HOST_IP]]/g' istio/addons/servicegraph.yaml"
