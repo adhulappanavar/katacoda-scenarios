73fe8f93b6d48f929915565259125708c3f8105d
Anil Dhulappanavar <anil.dhulappanavar.2015@gmail.com>
2019-10-07 10:28:44 +0530

    Installing Jhipster to Course Base

--- istio-scenarios/deploy-istio-on-kubernetes/courseBase.sh
+++ istio-scenarios/deploy-istio-on-kubernetes/courseBase.sh
@@ -1,2 +1,4 @@
 ssh root@host01 "git clone https://github.com/adhulappanavar/katacoda-scenarios.git; mv katacoda-scenarios/istio/assets/ istio; rm -rf katacoda-scenarios"
 ssh root@host01 "sed -i 's/\$HOST_IP/[[HOST_IP]]/g' istio/all.yaml; sed -i 's/\$HOST_IP/[[HOST_IP]]/g' istio/istio.yaml; sed -i 's/\$HOST_IP/[[HOST_IP]]/g' istio/istio-auth.yaml; sed -i 's/\$HOST_IP/[[HOST_IP]]/g' istio/addons/grafana.yaml; sed -i 's/\$HOST_IP/[[HOST_IP]]/g' istio/addons/zipkin.yaml; sed -i 's/\$HOST_IP/[[HOST_IP]]/g' istio/addons/servicegraph.yaml"
+ssh root@host01 "npm install -g generator-jhipster"
+
